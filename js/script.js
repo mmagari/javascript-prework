29e8f05334268630104b9bc9f8c72d91979fc776
Jarek.Mankiewicz <mmagari@wp.pl>
2023-04-26 12:52:25 +0200

    Add jsvsdcript game with buttons

--- js/script.js
+++ js/script.js
@@ -1,18 +1,66 @@
-
+//funkcja porównująca ruch gracza z ruchem komputera
 function playGame(){
-    var playerMove2, computerMove2;
-    computerMove2 = draw();
-    playerMove2 = playerMoveFunction();
+    var argButtonName, buttonPlay;
+    buttonPlay = draw();
+    argButtonName = playerMoveFunction();
     
-if (playerMove2 == computerMove2) {
-    printMessage('Twój ruch: ' + playerMove2 + ' Ruch komputera to: ' + computerMove2 + 'Remis');
-}   else if( playerMove2 == 'kamień' && computerMove2 == 'nożyce'){
-    printMessage('Twój ruch: ' + playerMove2 + ' Ruch komputera to: ' + computerMove2 + '<br> Gratulacje - Wygrałeś');
-}   else if( playerMove2 == 'nożyce' && computerMove2 == 'papier'){ 
-    printMessage('Twój ruch: ' + playerMove2 + ' Ruch komputera to: ' + computerMove2 + '<br> Gratulacje - Wygrałeś');
-}   else if( playerMove2 == 'papier' && computerMove2 == 'kamień'){   
-    printMessage('Twój ruch: ' + playerMove2 + ' Ruch komputera to: ' + computerMove2 + '<br> Gratulacje - Wygrałeś');
+if (argButtonName == buttonPlay) {
+    printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + buttonPlay + 'Remis');
+}   else if( argButtonName == 'kamień' && buttonPlay == 'nożyce'){
+    printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + buttonPlay + '<br> Gratulacje - Wygrałeś');
+}   else if( argButtonName == 'nożyce' && buttonPlay == 'papier'){ 
+    printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + buttonPlay + '<br> Gratulacje - Wygrałeś');
+}   else if( argButtonName == 'papier' && buttonPlay == 'kamień'){   
+    printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + buttonPlay + '<br> Gratulacje - Wygrałeś');
 }   else {
-    printMessage('Twój ruch: ' + playerMove2 + ' Ruch komputera to: ' + computerMove2 +' <br> Niestety przegrałeś.');
+    printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + buttonPlay +' <br> Niestety przegrałeś.');
     }
 }
+
+//**funckja testowa, na użycie przycisków
+//var argButtonName, buttonPlay;
+
+//function buttonClicked(argButtonName){
+    //clearMessages();
+//    console.log(argButtonName + ' został kliknięty');
+//}
+
+//buttonPlay = document.getElementById('button-rock');
+//buttonPlay.addEventListener('click', function(){ buttonClicked('Guzik kamień'); });
+
+//buttonPlay = document.getElementById('button-paper');
+//buttonPlay.addEventListener('click', function(){ buttonClicked('Guzik paper'); });
+
+//buttonPlay = document.getElementById('button-scissors');
+//buttonPlay.addEventListener('click', function(){ buttonClicked('Guzik nożyce'); });
+
+
+//funckja porównująca ruchy, z użyciem przycisków
+var argButtonName, argCompMove, argCompMove;
+
+function buttonClicked(argButtonName){
+    console.log(argButtonName + ' został kliknięty');
+    argCompMove = draw();
+    if (argButtonName == argCompMove) {
+        printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + argCompMove + '<br> Remis');
+    }   else if( argButtonName == 'kamień' && argCompMove == 'nożyce'){
+        printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + argCompMove + '<br> Gratulacje - Wygrałeś');
+    }   else if( argButtonName == 'nożyce' && argCompMove == 'papier'){ 
+        printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + argCompMove + '<br> Gratulacje - Wygrałeś');
+    }   else if( argButtonName == 'papier' && argCompMove == 'kamień'){   
+        printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + argCompMove + '<br> Gratulacje - Wygrałeś');
+    }   else {
+        printMessage('Twój ruch: ' + argButtonName + ' Ruch komputera to: ' + argCompMove +' <br> Niestety przegrałeś.');
+        }
+    }
+
+
+argCompMove = document.getElementById('button-rock');
+argCompMove.addEventListener('click', function(){ buttonClicked('kamień'); });
+
+argCompMove = document.getElementById('button-paper');
+argCompMove.addEventListener('click', function(){ buttonClicked('paper'); });
+
+argCompMove = document.getElementById('button-scissors');
+argCompMove.addEventListener('click', function(){ buttonClicked('nożyce'); });
+
